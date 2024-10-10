5a36c21d19194c170731a4de0f89efbfc042164f
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2024-10-10 17:54:19 +0500

    Initial commit

---
+++ portfolio/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+handler404 = handler404
+
+urlpatterns = [
+
+    path('', homePage, name='homePage'),
+    path('projects/', projectsPage, name='projectsPage'),
+    path('projects/<str:slug>/', projectDetail, name='projectDetail'),
+    path('search/', search, name='search'),
+
+    path('dashboard/', include('dashboard.urls')),
+    # path('admin/', admin.site.urls),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
